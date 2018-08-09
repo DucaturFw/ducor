b71764ceaccf3aa6bb216405474b7cdb79761c6d
Anatoly Bardukov <sindbag@gmail.com>
2018-08-09 14:48:09 +0300

    Readme and commands refactoring

--- update.sh
+++ update.sh
@@ -1,3 +1,2 @@
 git pull --recurse-submodules
 docker-compose -f docker-compose.yml -f docker-compose.prod_override.yml build
-docker-compose -f docker-compose.yml -f docker-compose.prod_override.yml up -d
